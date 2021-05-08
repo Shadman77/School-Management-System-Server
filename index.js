5e38c5811b742bac72d45d24734a3e94d0f811fe
Shadman Saif Anonno <shadmansaif@outlook.com>
2021-05-08 19:39:18 +0600

    Last class

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
 
+// Using middleware for json support
+app.use(express.json());
+
 app.get("/", (req, res) => {
   return res.json({
     msg: "Hello World",
+  });
+});
+
+app.get("/:msg", (req, res) => {
+  console.log(req.params.msg);
+  return res.json({
+    msg: req.params.msg,
+  });
+});
+
+app.post("/upload", (req, res) => {
+  console.log(req.body);
+  return res.json({
+    msg: "Data Recieved",
   });
 });
 
