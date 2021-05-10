1303bae2b86a74594c82241029c9363a774d7696
Shadman Saif Anonno <shadmansaif@outlook.com>
2021-05-11 00:13:13 +0600

    exception handling

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -45,34 +45,29 @@ const validate_register = require("../../middleware/validation/auth/register");
 
 router.post("/register", validate_register, async (req, res) => {
   const { email, password, password_confirm } = req.body;
-  // console.log(email, password, password_confirm);
+
+  const newUser = new User({
+    email,
+    password,
+    password_confirm,
+  });
 
   try {
     const user = await User.findOne({ email });
 
-    if (user) {
-      return res.json({
-        msg: "Email already exists",
-      });
-    }
-
-    const newUser = new User({
-      email,
-      password,
-      password_confirm,
-    });
+    if (user) throw Error("Email already exists");
 
     const savedUser = await newUser.save();
-    if (savedUser) {
-      return res.json({
-        msg: "User saved",
-      });
-    } else {
-      return res.json({
-        msg: "User could not be saved",
-      });
-    }
-  } catch (err) {}
+    if (!savedUser) throw Error("User could not be saved");
+
+    return res.json({
+      msg: "User saved",
+    });
+  } catch (err) {
+    return res.json({
+      msg: err.message,
+    });
+  }
 });
 
 module.exports = router;
